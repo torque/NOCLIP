0afad6b58521c4e85c64cd7f250c1130a64c4479
torque <torque@users.noreply.github.com>
2023-03-20 19:54:13 -0700

    build.zig: rework for build system changes
    At some point I will also figure out the proper way to make this a
    module, I guess.

--- build.zig
+++ build.zig
@@ -4,24 +4,33 @@ pub fn build(b: *std.build.Builder) void {
     const demo = b.step("demo", "noclip demo");
     const tests = b.step("test", "Run unit tests");
 
-    // b.use_stage1 = false;
     const target = b.standardTargetOptions(.{});
-    const mode = b.standardReleaseOptions();
+    const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addSharedLibrary("noclip", "source/noclip.zig", .unversioned);
-
-    exe.setTarget(target);
-    exe.setBuildMode(mode);
+    const exe = b.addSharedLibrary(.{
+        .name = "noclip",
+        .root_source_file = .{ .path = "source/noclip.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
     exe.install();
 
-    const demo_exe = b.addExecutable("noclip-demo", "demo/demo.zig");
-    demo_exe.addPackagePath("noclip", "source/noclip.zig");
+    const demo_exe = b.addExecutable(.{
+        .name = "noclip-demo",
+        .root_source_file = .{ .path = "demo/demo.zig" },
+    });
+    demo_exe.addModule("noclip", b.createModule(.{
+        .source_file = .{ .path = "source/noclip.zig" },
+    }));
     const install_demo = b.addInstallArtifact(demo_exe);
     demo.dependOn(&install_demo.step);
 
-    const lib_tests = b.addTest("source/noclip.zig");
-    lib_tests.setTarget(target);
-    lib_tests.setBuildMode(mode);
+    const lib_tests = b.addTest(.{
+        .name = "tests",
+        .root_source_file = .{ .path = "source/noclip.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
 
     tests.dependOn(&lib_tests.step);
 }
