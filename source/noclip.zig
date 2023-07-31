d1803284b4c9c1c19ff727f4c9d1c0267cacec8c
torque <torque@users.noreply.github.com>
2023-07-31 12:40:37 -0700

    noclip: repub imports
    As far as I can tell, there's no good reason not to do this.

--- source/noclip.zig
+++ source/noclip.zig
@@ -1,9 +1,10 @@
-const command = @import("./command.zig");
-const converters = @import("./converters.zig");
-const errors = @import("./errors.zig");
-const help = @import("./help.zig");
-const ncmeta = @import("./meta.zig");
-const parameters = @import("./parameters.zig");
-const parser = @import("./parser.zig");
+pub const command = @import("./command.zig");
+pub const converters = @import("./converters.zig");
+pub const errors = @import("./errors.zig");
+pub const help = @import("./help.zig");
+pub const ncmeta = @import("./meta.zig");
+pub const parameters = @import("./parameters.zig");
+pub const parser = @import("./parser.zig");
 
 pub const CommandBuilder = command.CommandBuilder;
+pub const ParserInterface = parser.ParserInterface;
